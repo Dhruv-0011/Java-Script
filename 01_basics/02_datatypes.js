f9755aa6a73fefabb553fd1653d96210595a6072
Dhruv-0011 <128117948+Dhruv-0011@users.noreply.github.com>
2024-05-30 11:49:49 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"  //treat all the code as a new version of JS.
+
+// alert(3+3) //throw error as we are using node not browser
+
+console.log(3+3)
+console.log("3"+3)
+
+let name = "Dhruv"
+let isLoggedIn = true
+let age = 20
+
+/*
+    integer => 2^52
+    bigint
+    string => "" || ''
+    boolean => true || false
+    null => standalone value
+    undefined => when val is not assigned till now....
+    symbol => uniqueness...
+
+    object
+
+*/
+
+console.log(typeof(3))
+console.log(typeof(isLoggedIn))
+console.log(typeof(null))
