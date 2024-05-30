d2b926513af68c313edb5b0cc4080a573016ec5a
Dhruv-0011 <128117948+Dhruv-0011@users.noreply.github.com>
2024-05-30 12:49:38 +0000

    conversion

---
+++ 01_basics/03_conversionsWithOperations.js
@@ -0,0 +1,29 @@
+let score = null
+
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+/*
+    "33" => 33 Number
+    "dhruv" => NaN(Not a Number)
+    "true" => 1
+    "null" => 0
+*/
+let chk = 2
+console.log(Boolean(chk))
+/*
+    0 => false; else => true
+*/
+let someNumber = 33
+let stringNumber = String(someNumber);
+// console.log(someNumber)
+// console.log(typeof stringNumber)
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+console.log(2**3)
